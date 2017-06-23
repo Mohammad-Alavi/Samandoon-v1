d1ef9eb164149870e9be9096ce194d47a0f5fbd6
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2017-06-24 04:23:31 +0430

    Created Event Factory

---
+++ app/Containers/Event/Data/Factories/EventFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+$factory->define(App\Containers\Event\Models\Event::class, function (Faker\Generator $faker) {
+
+    return [
+        'title'     => $faker->title,
+        'description'    => $faker->realText(),
+        'event_date' => $faker->dateTime($format = 'Y-m-d-H-i-T'),
+    ];
+});
