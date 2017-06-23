d799a0b07e0e6c5c1556880f39bfeecd898968c9
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2017-06-24 04:25:32 +0430

    Updated User Factory

--- app/Containers/User/Data/Factories/UserFactory.php
+++ app/Containers/User/Data/Factories/UserFactory.php
@@ -5,8 +5,14 @@ use Illuminate\Support\Facades\Hash;
 $factory->define(App\Containers\User\Models\User::class, function (Faker\Generator $faker) {
 
     return [
-        'name'     => $faker->name,
+        'first_name'     => $faker->firstName,
+        'last_name'     => $faker->lastName,
         'email'    => $faker->email,
         'password' => Hash::make('testing-password'),
+        'confirmed'     => $faker->boolean(),
+        'device'     => str_random(7),
+        'platform'     => $faker->randomElement(['android', 'ios', 'web', 'desktop']),
+        'gender'     => $faker->randomElement(['male', 'female']),
+        'birth'     => $faker->date($format = 'Y-m-d', $max = 'now'),
     ];
 });
