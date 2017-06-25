af7f879f3c32aec08b83cec53d8257568e07f03a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2017-06-25 17:10:17 +0430

    Updated seeder

--- app/Ship/Seeders/Data/Testing/Seeders/TestingDataSeeder.php
+++ app/Ship/Seeders/Data/Testing/Seeders/TestingDataSeeder.php
@@ -3,6 +3,7 @@
 namespace App\Ship\Seeders\Data\Testing\Seeders;
 
 use App\Containers\Event\Models\Event;
+use App\Containers\NGO\Models\NGO;
 use App\Containers\User\Models\User;
 use App\Ship\Parents\Seeders\Seeder;
 
@@ -21,8 +22,22 @@ class TestingDataSeeder extends Seeder
      */
     public function run()
     {
-        factory(Event::class, 500)->create();
-        factory(User::class, 500)->create();
+//        factory(NGO::class, 50)
+//            ->create()
+//            ->each(function ($u){
+//                $u->events()->saveMany(factory(Event::class, rand(2, 11))->make());
+//            });
+
+//        factory(Event::class, 100)->create();
+
+        factory(User::class, 50)
+            ->create()
+            ->each(function ($u){
+            $u->ngos()->saveMany(factory(NGO::class, rand(0, 3))->make())
+                ->each(function ($u){
+                $u->events()->saveMany(factory(Event::class, rand(2, 11))->make());
+            });;
+        });
     }
 
 }
