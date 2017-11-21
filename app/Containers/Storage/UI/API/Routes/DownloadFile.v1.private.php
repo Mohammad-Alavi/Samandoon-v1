4a6de64e57412932be7c38f1767b56506608912b
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2017-11-21 04:40:09 +0330

    Added Docs

--- app/Containers/Storage/UI/API/Routes/DownloadFile.v1.private.php
+++ app/Containers/Storage/UI/API/Routes/DownloadFile.v1.private.php
@@ -9,7 +9,8 @@
  * @apiVersion         1.0.0
  * @apiPermission      none
  *
- * @apiUse             UserSuccessSingleResponse
+ * @apiSuccessExample  {json}       Success-Response:
+ * HTTP/1.1 202 OK
  */
 
 
