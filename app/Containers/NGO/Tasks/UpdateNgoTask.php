41a2c7004e31dce6300dc247ad8482e438084d13
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2017-11-21 06:01:38 +0330

    Update: delete prev pic before updating to new pic

--- app/Containers/NGO/Tasks/UpdateNgoTask.php
+++ app/Containers/NGO/Tasks/UpdateNgoTask.php
@@ -2,10 +2,12 @@
 
 namespace App\Containers\NGO\Tasks;
 
+use Apiato\Core\Foundation\Facades\Apiato;
 use App\Containers\NGO\Data\Repositories\NGORepository;
 use App\Ship\Exceptions\UpdateResourceFailedException;
 use App\Ship\Parents\Tasks\Task;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Storage;
 
 class UpdateNgoTask extends Task
 {
@@ -13,6 +15,14 @@ class UpdateNgoTask extends Task
     {
         if (empty($ngoData)) {
             throw new UpdateResourceFailedException('Inputs are empty.');
+        }
+
+        if ($ngoData->logo_photo_path) {
+            Storage::disk('public')->delete('ngo_logo'.'/'.Apiato::call('NGO@FindNgoByIdTask', $ngoId)->logo_photo_path);
+        }
+
+        if ($ngoData->banner_photo_path !== null) {
+            Storage::disk('public')->delete('ngo_banner'.'/'.Apiato::call('NGO@FindNgoByIdTask', $ngoId)->logo_banner_path);
         }
 
         return App::make(NgoRepository::class)->update($ngoData, $ngoId);
