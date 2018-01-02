dbc4d18ffb3aa497d6bfefb99d3dc143a3a0b643
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-01-02 21:06:50 +0330

    Fix DL UL Media

--- app/Containers/Storage/Tasks/DownloadFileTask.php
+++ app/Containers/Storage/Tasks/DownloadFileTask.php
@@ -3,17 +3,16 @@
 namespace App\Containers\Storage\Tasks;
 
 use App\Ship\Parents\Tasks\Task;
-use Spatie\MediaLibrary\Media;
 
 class DownloadFileTask extends Task
 {
     public function run($request)
     {
-        $media =  media::find($request->id);
+        $file =  'storage/' . $request->id . '/' . $request->resource_name;
 //        switch ($request->model_type) {
 //            case 'ngo':
 //            $media = Ngo::find($request->id)->getMedia('');
 //        }
-        return $media;
+        return $file;
     }
 }
