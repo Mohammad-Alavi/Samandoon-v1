9ca20ff1c905be5e2a070bc9b07fe0c9e9d05694
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-02-25 15:58:22 +0330

    updated vaidations

--- app/Containers/User/Actions/FollowFeedAction.php
+++ app/Containers/User/Actions/FollowFeedAction.php
@@ -3,6 +3,7 @@
 namespace App\Containers\User\Actions;
 
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Ship\Exceptions\ValidationFailedException;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 
@@ -10,6 +11,10 @@ class FollowFeedAction extends Action
 {
     public function run(DataTransporter $dataTransporter)
     {
-         return Apiato::call('User@FollowFeedTask', [$dataTransporter]);
+        // Check if the following and follower ID's are the same and throw an exception
+        $user = Apiato::call('Authentication@GetAuthenticatedUserTask');
+        info($user->getHashedKey() . ' | ' . $dataTransporter->id);
+        throw_if($user->getHashedKey() == $dataTransporter->id, new ValidationFailedException('Both Following and Follower id\'s are the same. User cannot follow itself!'));
+        return Apiato::call('User@FollowFeedTask', [$dataTransporter]);
     }
 }
