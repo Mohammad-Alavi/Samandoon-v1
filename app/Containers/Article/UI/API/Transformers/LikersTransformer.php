15056573c24279b8b718912d10203e04b318346a
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-04-16 15:04:40 +0430

    updated likers transformer

--- app/Containers/Article/UI/API/Transformers/LikersTransformer.php
+++ app/Containers/Article/UI/API/Transformers/LikersTransformer.php
@@ -9,24 +9,22 @@ class LikersTransformer
         $tempArray = [];
         foreach ($users as $user) {
             $response = [
-                'object' => [
-                    'object' => 'User',
-                    'id' => $user->getHashedKey(),
-                    'first_name' => $user->first_name,
-                    'last_name' => $user->last_name,
-                    'email' => $user->email,
-                    'avatar' => empty($user->getFirstMediaUrl('avatar')) ?
-                        'http://api.' . str_replace('http://', '', config('app.url')) . '/v1/storage' . config('samandoon.default.avatar') :
-                        'http://api.' . str_replace('http://', '', config('app.url')) . '/v1' . str_replace(str_replace('http://', '', config('app.url')), '', $user->getFirstMediaUrl('avatar')),
-                    'confirmed' => $user->confirmed,
-                    'gender' => $user->gender,
-                    'birth' => $user->birth,
-                    'ngo_id' => $user->ngo->id ? $user->ngo->getHashedKey() : null,
+                'object' => 'User',
+                'id' => $user->getHashedKey(),
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'email' => $user->email,
+                'avatar' => empty($user->getFirstMediaUrl('avatar')) ?
+                    'http://api.' . str_replace('http://', '', config('app.url')) . '/v1/storage' . config('samandoon.default.avatar') :
+                    'http://api.' . str_replace('http://', '', config('app.url')) . '/v1' . str_replace(str_replace('http://', '', config('app.url')), '', $user->getFirstMediaUrl('avatar')),
+                'confirmed' => $user->confirmed,
+                'gender' => $user->gender,
+                'birth' => $user->birth,
+                'ngo_id' => $user->ngo->id ? $user->ngo->getHashedKey() : null,
 
-                    'view_user' => [
-                        'href' => 'v1/user/' . $user->getHashedKey(),
-                        'method' => 'GET'
-                    ]
+                'view_user' => [
+                    'href' => 'v1/user/' . $user->getHashedKey(),
+                    'method' => 'GET'
                 ],
             ];
 
