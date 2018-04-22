176651c1aeb08a6861fb03eb4b4f8c19837ebceb
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-04-22 05:19:26 +0430

    fixed comment transformer

--- app/Containers/Article/UI/API/Transformers/CommentTransformer.php
+++ app/Containers/Article/UI/API/Transformers/CommentTransformer.php
@@ -72,53 +72,43 @@ class CommentTransformer
             }
 
             $response = [
-                'object' => [
-                    'object' => 'Comment',
-                    'id' => Hashids::encode($comments->id),
-                    'body' => $comments->body,
-                    'commentable_id' => Hashids::encode($comments->commentable_id),
-                    'commentable_type' => $comments->commentable_type,
-                    'creator_id' => Hashids::encode($comments->creator_id),
-                    'creator_type' => 'User',
-                    'creator_data' => [
-                        'first_name' => $creator_data->first_name,
-                        'last_name' => $creator_data->last_name,
-                        'avatar' => empty($creator_data->getFirstMediaUrl('avatar')) ?
-                            'http://api.' . str_replace('http://', '', config('app.url')) . '/v1/storage' . config('samandoon.default.avatar_thumb') :
-                            'http://api.' . str_replace('http://', '', config('app.url')) . '/v1' . str_replace(str_replace('http://', '', config('app.url')), '', $creator_data->getFirstMedia('avatar')->getUrl('thumb')),
-                        'ngo_data' => [
-                            'ngo_id' => $creator_data->ngo->id ? $creator_data->ngo->getHashedKey() : null,
-                            'name' => $creator_data->ngo->id ? $creator_data->ngo->name : null,
-                            'confirmed' => $creator_data->ngo->id ? $creator_data->ngo->confirmed : null,
-                        ]
-                    ],
-                    '_lft' => $comments->_lft,
-                    '_rgt' => $comments->_rgt,
-                    'parent_id' => is_null($comments->parent_id) ? null : Hashids::encode($comments->parent_id),
-                    'created_at' => $comments->created_at,
-                    'updated_at' => $comments->updated_at,
-
-                    'view_comment' => [
-                        'href' => 'v1/ngo/article/comment/' . Hashids::encode($comments->id),
-                        'method' => 'GET'
+                'object' => 'Comment',
+                'id' => Hashids::encode($comments->id),
+                'body' => $comments->body,
+                'commentable_id' => Hashids::encode($comments->commentable_id),
+                'commentable_type' => $comments->commentable_type,
+                'creator_id' => Hashids::encode($comments->creator_id),
+                'creator_type' => 'User',
+                'creator_data' => [
+                    'first_name' => $creator_data->first_name,
+                    'last_name' => $creator_data->last_name,
+                    'avatar' => empty($creator_data->getFirstMediaUrl('avatar')) ?
+                        'http://api.' . str_replace('http://', '', config('app.url')) . '/v1/storage' . config('samandoon.default.avatar_thumb') :
+                        'http://api.' . str_replace('http://', '', config('app.url')) . '/v1' . str_replace(str_replace('http://', '', config('app.url')), '', $creator_data->getFirstMedia('avatar')->getUrl('thumb')),
+                    'ngo_data' => [
+                        'ngo_id' => $creator_data->ngo->id ? $creator_data->ngo->getHashedKey() : null,
+                        'name' => $creator_data->ngo->id ? $creator_data->ngo->name : null,
+                        'confirmed' => $creator_data->ngo->id ? $creator_data->ngo->confirmed : null,
                     ]
                 ],
+                '_lft' => $comments->_lft,
+                '_rgt' => $comments->_rgt,
+                'parent_id' => is_null($comments->parent_id) ? null : Hashids::encode($comments->parent_id),
+                'created_at' => $comments->created_at,
+                'updated_at' => $comments->updated_at,
+
+                'view_comment' => [
+                    'href' => 'v1/ngo/article/comment/' . Hashids::encode($comments->id),
+                    'method' => 'GET'
+                ]
             ];
 
             array_push($tempArray, $response);
 
             $data = [
                 'data' => $tempArray,
-                'meta' => [
-                    'pagination' => [
-                        'per_page' => null,
-                        'current_page' => null,
-                        'total_pages' => null,
-                    ]
-                ]
             ];
         }
-
 
         return $data;
     }
