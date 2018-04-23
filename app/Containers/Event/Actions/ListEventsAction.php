df326226199e1ba756e43f0b9d7b14dd549c6e37
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-04-23 07:04:14 +0430

    code refactor

--- app/Containers/Event/Actions/ListEventsAction.php
+++ app/Containers/Event/Actions/ListEventsAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\Event\Actions;
 
+use Apiato\Core\Foundation\Facades\Apiato;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Parents\Requests\Request;
 
@@ -12,7 +13,9 @@ class ListEventsAction extends Action
         $whereInMethod = $request->has('field') && $request->has('value') ? ['where_in' => [$request->field, $request->value]] : [''];
         $withNgoIdMethod = $request->has('ngo_id') ? ['ngo_id' => [$request->ngo_id]] : [''];
 
-        $events = $this->call('Event@ListEventsTask', [], [
+        $events = Apiato::call('Event@ListEventsTask', [], [
+            //call ordeyBy method by default
+            //if there is no parameters some default params will be used instead
             ['orderBy' => [$request->orderBy, $request->sortedBy]],
             $withNgoIdMethod,
             $whereInMethod
