a4a2da9cbb20d2453faa937f7dedca73bb878f34
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-05-01 08:43:58 +0430

    added UTF-8 char set

--- app/Containers/User/UI/WEB/Views/user-welcome.blade.php
+++ app/Containers/User/UI/WEB/Views/user-welcome.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
