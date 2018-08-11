a8654390281da9be288b2df545e9abcf6f89de15
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-12 04:24:48 +0430

    Update Docs

--- app/Containers/NGO/UI/API/Routes/UpdateNgo.v1.private.php
+++ app/Containers/NGO/UI/API/Routes/UpdateNgo.v1.private.php
@@ -18,8 +18,7 @@
  * @apiParam           {String}  [zip_code] max:255
  * @apiParam           {image}  [ngo_logo]
  * @apiParam           {image}  [ngo_banner]
- * @apiParam           {json}  [phone] If you send an empty id -> "" a new phone record will be created. But if you send an id, if you have access to it then it will be updated otherwise an exception will be thrown at yo bitch face :| Example: [{"id" : "", "label" : "testLabel", "phone_number" : "06153224740"},
-                                        {"id" : "5", "label" : "testLabel", "phone_number" : "06153224746"}]
+ * @apiParam           {json}  [phone] Example: [{"label" : "خانه", "phone_number" : "06153224740"}, {"label" : "دفتر", "phone_number" : "06153224746"}]
  * @apiParam           {json}  [subject] To delete all subjects send an empty array -> []. To add or update subject send an array with subject ID's -> [1,3]. This subjects will replace the current subjects of NGO. e.g. if current subjects are [1,2] and you send [1,3], new subjects will be [1,3].
  *
  * @apiUse             NGOSuccessSingleResponse
