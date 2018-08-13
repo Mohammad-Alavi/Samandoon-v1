ea29f395a135d984f85bbb20af88f90439d9d6b4
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-14 01:56:48 +0430

    Change FCM transfomer

--- app/Containers/FCM/UI/API/Transformers/UserFCMTokenTransformer.php
+++ app/Containers/FCM/UI/API/Transformers/UserFCMTokenTransformer.php
@@ -9,8 +9,8 @@ class UserFCMTokenTransformer
     public function transform($entity)
     {
         $response = [
-            'object' => 'UserFCMToken',
-            'id' => Hashids::encode($entity->id),
+//            'object' => 'UserFCMToken',
+//            'id' => Hashids::encode($entity->id),
             'user_id' => Hashids::encode($entity->user_id),
             'android_fcm_token' => $entity->android_fcm_token[0],
             'apns_id' => $entity->apns_id[0],
