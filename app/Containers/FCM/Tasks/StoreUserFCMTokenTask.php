3b94920ac9fe38ec6eb1440bb9236630783888c6
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-16 04:37:00 +0430

    Bug fix

--- app/Containers/FCM/Tasks/StoreUserFCMTokenTask.php
+++ app/Containers/FCM/Tasks/StoreUserFCMTokenTask.php
@@ -21,15 +21,28 @@ class StoreUserFCMTokenTask extends Task
         try {
             if (array_key_exists('device_type', $data) && $data['device_type'] == 'ios') {
                 $FCMTokenData = UserFCMToken::firstOrCreate([
+                    'apns_id' => $data['token']
+                ], [
                     'user_id' => $userId,
                     'apns_id' => $data['token']
                 ]);
             } else {
                 $FCMTokenData = UserFCMToken::firstOrCreate([
+                    'android_fcm_token' => $data['token']
+                ], [
                     'user_id' => $userId,
                     'android_fcm_token' => $data['token']
                 ]);
             }
+
+            if (strlen($FCMTokenData->android_fcm_token[0]) > 1) {
+                $FCMTokenData->android_fcm_token = $FCMTokenData->android_fcm_token[0];
+            }
+
+            if (strlen($FCMTokenData->apns_id[0]) > 1) {
+                $FCMTokenData->apns_id = $FCMTokenData->apns_id[0];
+            }
+
             return $FCMTokenData;
         } catch (Exception $exception) {
             throw new CreateResourceFailedException('Failed to store new Token with error: ' . $exception->getMessage());
