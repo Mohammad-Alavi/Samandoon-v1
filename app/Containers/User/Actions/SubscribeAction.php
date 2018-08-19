ea3f615125ba977e7d0951593ef95421dc4e9999
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-19 06:22:11 +0430

    Do not allow user to follow his own NGO

--- app/Containers/User/Actions/SubscribeAction.php
+++ app/Containers/User/Actions/SubscribeAction.php
@@ -5,6 +5,7 @@ namespace App\Containers\User\Actions;
 use App\Ship\Parents\Actions\Action;
 use Apiato\Core\Foundation\Facades\Apiato;
 use App\Ship\Transporters\DataTransporter;
+use Illuminate\Validation\UnauthorizedException;
 
 class SubscribeAction extends Action
 {
@@ -12,6 +13,10 @@ class SubscribeAction extends Action
     {
         $AuthenticatedUser = Apiato::call('Authentication@GetAuthenticatedUserTask');
         $ngo = Apiato::call('NGO@FindNgoByIdTask', [$dataTransporter->id]);
+
+        // throw if user is trying to subscribe to his own ngo
+        throw_if($ngo->id == $AuthenticatedUser->ngo->id, UnauthorizedException::class, 'User cannot follow is own NGO');
+
         $result = Apiato::call('User@SubscribeTask', [$AuthenticatedUser, $ngo]);
         return $result;
     }
