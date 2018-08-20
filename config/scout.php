891534b99728955668d9731fc181a1ad735852db
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-21 03:46:50 +0430

    Improve search

--- config/scout.php
+++ config/scout.php
@@ -79,9 +79,9 @@ return [
         'storage'  => storage_path(), //place where the index files will be stored
         'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
         'fuzzy' => [
-            'prefix_length' => 2,
+            'prefix_length' => 4,
             'max_expansions' => 50,
-            'distance' => 7
+            'distance' => 8
         ],
         'asYouType' => false,
         'searchBoolean' => env('TNTSEARCH_BOOLEAN', false),
