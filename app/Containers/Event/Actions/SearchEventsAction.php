aeeffbf3201d5dfe7e0445181a27c83641ff3d0d
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-23 08:21:51 +0430

    Add search filters

--- app/Containers/Event/Actions/SearchEventsAction.php
+++ app/Containers/Event/Actions/SearchEventsAction.php
@@ -2,7 +2,7 @@
 
 namespace App\Containers\Event\Actions;
 
-use App\Containers\Event\Models\Event;
+use Apiato\Core\Foundation\Facades\Apiato;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Transporters\DataTransporter;
 
@@ -10,6 +10,14 @@ class SearchEventsAction extends Action
 {
     public function run(DataTransporter $data)
     {
-         return $this->call('Event@SearchEventsTask', [$data]);
+        $sanitizedData = $data->sanitizeInput([
+            'q',
+            'area_of_activity', // ngo
+            'subject', // ngo
+            'city', // event
+            'province', // event
+        ]);
+
+         return Apiato::call('Event@SearchEventsTask', [$sanitizedData, $data->limit]);
     }
 }
--- app/Containers/Event/Tasks/SearchEventsTask.php
+++ app/Containers/Event/Tasks/SearchEventsTask.php
@@ -3,13 +3,74 @@
 namespace App\Containers\Event\Tasks;
 
 use App\Containers\Event\Models\Event;
+use App\Containers\NGO\Models\Ngo;
+use App\Containers\NGO\Tasks\ConvertNGONameFromArabicToPersianTask;
 use App\Ship\Parents\Tasks\Task;
-use App\Ship\Transporters\DataTransporter;
 
 class SearchEventsTask extends Task
 {
-    public function run(DataTransporter $data)
+    public function run(array $request, $limit = 10)
     {
-        return Event::Search($data->q)->paginate($data->limit);
+        $whereFilter = array();
+        $subjectsArray = [];
+        foreach ($request as $key => $value) {
+            if ($key == 'area_of_activity') {
+                $whereFilter[] = [$key, '=', $value];
+            }
+
+            if ($key == 'subject') {
+                $subjectsArray = json_decode($value, true);
+            }
+        }
+
+        // here we get filtered ngos
+        $filteredNgos = [];
+        if (!empty($subjectsArray) && !empty($whereFilter)) {
+            $filteredNgos = Ngo::where($whereFilter)->whereHas('subjects', function ($query) use ($subjectsArray) {
+                $query->whereIn('subject_id', $subjectsArray);
+            })->get();
+        }
+        if (!empty($subjectsArray) && empty($whereFilter)) {
+            $filteredNgos = Ngo::whereHas('subjects', function ($query) use ($subjectsArray) {
+                $query->whereIn('subject_id', $subjectsArray);
+            })->get();
+        }
+        if (empty($subjectsArray) && !empty($whereFilter)) {
+            $filteredNgos = Ngo::where($whereFilter)->get();
+        }
+
+        $eventIdArray = [];
+        foreach ($filteredNgos as $ngo) {
+            foreach ($ngo->events as $event) {
+                $eventIdArray[] = $event->id;
+            }
+        }
+
+        // get city and province for event
+        $whereFilterEvent = array();
+        foreach ($request as $key => $value) {
+            if ($key == 'city' || $key == 'province') {
+                $whereFilterEvent[] = [$key, '=', $value];
+            }
+        }
+
+        if (!empty($whereFilterEvent)) {
+            $filteredEvent = Event::where($whereFilterEvent)->get();
+            foreach ($filteredEvent as $event) {
+                $eventIdArray[] = $event->id;
+            }
+        }
+
+        $eventIdArray = array_unique($eventIdArray);
+
+        $filteredEvent = Event::whereIn('id', $eventIdArray);
+
+        if (array_key_exists('q', $request) && $request['q'] != '') {
+            $result = Event::Search(ConvertNGONameFromArabicToPersianTask::arabicToPersian($request['q']))->constrain($filteredEvent)->paginate($limit);
+        } else {
+            $result = $filteredEvent->paginate($limit);
+        }
+
+        return $result;
     }
 }
