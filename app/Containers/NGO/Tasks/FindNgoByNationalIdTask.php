0354a887e834db77886146e0adc63c93d1463371
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-08-29 18:29:12 +0430

    Improve exception handling and details

--- app/Containers/NGO/Tasks/FindNgoByNationalIdTask.php
+++ app/Containers/NGO/Tasks/FindNgoByNationalIdTask.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Containers\NGO\Tasks;
-use App\Ship\Exceptions\NotFoundException;
+
 use App\Ship\Parents\Tasks\Task;
 
 class FindNGOByNationalIdTask extends Task
@@ -13,15 +13,27 @@ class FindNGOByNationalIdTask extends Task
         ));
 
         $ch = curl_init();
+        //Tell cURL that it should only spend 10 seconds
+        //trying to connect to the URL in question.
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        //A given cURL operation should only take
+        //10 seconds max.
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+        curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
         curl_setopt($ch, CURLOPT_URL, 'http://irsherkat.ssaa.ir/hndlr/GeneralHandler.ashx?Id=34');
         curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $output = curl_exec($ch);
+        $curlError = curl_error($ch);
+        $curlErrno = curl_errno($ch);
         curl_close($ch);
         $result = json_decode($output, true);
-        if (empty($result['ResultList']['0']['NationalCode'])) {
-            throw new NotFoundException('NGO not found');
+//        if (empty($result['ResultList']['0']['NationalCode'])) {
+        if ($curlErrno) {
+            throw new \Exception($curlError, $curlErrno);
         } else {
             return $result;
         }
