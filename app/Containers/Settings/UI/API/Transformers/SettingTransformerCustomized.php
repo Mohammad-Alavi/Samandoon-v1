dee38407cbdf396a94fa4651968dfc9be97f4de3
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-09-26 15:26:32 +0330

    Minor changes

--- app/Containers/Settings/UI/API/Transformers/SettingTransformerCustomized.php
+++ app/Containers/Settings/UI/API/Transformers/SettingTransformerCustomized.php
@@ -11,13 +11,13 @@ class SettingTransformerCustomized
             "current_android_version_name" => (string)$entity['current_android_version_name'],
             "current_ios_version" => (int)$entity['current_ios_version'],
             "in_maintance_mode_message" => (string)$entity['in_maintance_mode_message'],
-            "is_in_maintance_mode" => (bool)$entity['is_in_maintance_mode'],
-            "is_login_allowed" => (bool)$entity['is_login_allowed'],
-            "is_new_article_allowed" => (bool)$entity['is_new_article_allowed'],
-            "is_new_event_allowed" => (bool)$entity['is_new_event_allowed'],
-            "is_ngo_creation_allowed" => (bool)$entity['is_ngo_creation_allowed'],
-            "is_payment_allowed" => (bool)$entity['is_payment_allowed'],
-            "is_registration_allowed" => (bool)$entity['is_registration_allowed'],
+            "is_in_maintance_mode" => $this->stringToBool($entity['is_in_maintance_mode']),
+            "is_login_allowed" => $this->stringToBool($entity['is_login_allowed']),
+            "is_new_article_allowed" => $this->stringToBool($entity['is_new_article_allowed']),
+            "is_new_event_allowed" => $this->stringToBool($entity['is_new_event_allowed']),
+            "is_ngo_creation_allowed" => $this->stringToBool($entity['is_ngo_creation_allowed']),
+            "is_payment_allowed" => $this->stringToBool($entity['is_payment_allowed']),
+            "is_registration_allowed" => $this->stringToBool($entity['is_registration_allowed']),
             "least_android_version_code" => (int)$entity['least_android_version_code'],
             "least_android_version_name" => (string)$entity['least_android_version_name'],
             "least_ios_version" => (int)$entity['least_ios_version'],
@@ -26,5 +26,9 @@ class SettingTransformerCustomized
         ];
 
         return $response;
+    }
+
+    function stringToBool(string $str) : bool {
+        return $str === 'true';
     }
 }
