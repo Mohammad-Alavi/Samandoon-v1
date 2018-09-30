04c549d6c55ed8e131f28098f3c7734ccbbf785e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-09-30 11:03:52 +0330

    Add pagination and sortBy orderBy

--- app/Containers/NGO/Actions/GetAllVerificationRequestsAction.php
+++ app/Containers/NGO/Actions/GetAllVerificationRequestsAction.php
@@ -4,12 +4,16 @@ namespace App\Containers\NGO\Actions;
 
 use App\Ship\Parents\Actions\Action;
 use Apiato\Core\Foundation\Facades\Apiato;
+use App\Ship\Transporters\DataTransporter;
 
 class GetAllVerificationRequestsAction extends Action
 {
-    public function run()
+    public function run(DataTransporter $request)
     {
-        $ngo = Apiato::call('NGO@GetAllVerificationRequestsTask', []);
-        return $ngo;
+        return Apiato::call('NGO@GetAllVerificationRequestsTask', [], [
+            ['orderBy' => [$request->orderBy, $request->sortedBy]],
+        ]);
+//        $ngo = Apiato::call('NGO@GetAllVerificationRequestsTask', []);
+//        return $ngo;
     }
 }
--- app/Containers/NGO/Tasks/GetAllVerificationRequestsTask.php
+++ app/Containers/NGO/Tasks/GetAllVerificationRequestsTask.php
@@ -3,6 +3,7 @@
 namespace App\Containers\NGO\Tasks;
 
 use App\Containers\NGO\Data\Repositories\NGORepository;
+use App\Ship\Criterias\Eloquent\OrderByFieldCriteria;
 use App\Ship\Parents\Tasks\Task;
 
 class GetAllVerificationRequestsTask extends Task
@@ -16,7 +17,12 @@ class GetAllVerificationRequestsTask extends Task
 
     public function run()
     {
-        $ngos = $this->repository->findByField(['verification_status' => config('samandoon.ngo_verification_status.requested')]);
+        $ngos = $this->repository->findByField(['verification_status' => config('samandoon.ngo_verification_status.requested')])->paginate();
         return $ngos;
+    }
+
+    public function orderBy($orderBy, $sortedBy)
+    {
+        $this->repository->pushCriteria(new OrderByFieldCriteria($orderBy, $sortedBy));
     }
 }
