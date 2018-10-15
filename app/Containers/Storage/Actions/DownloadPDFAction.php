774227634be8600d55143390ee729836c56e0a87
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2018-10-15 21:28:13 +0330

    Add validation

--- app/Containers/Storage/Actions/DownloadPDFAction.php
+++ app/Containers/Storage/Actions/DownloadPDFAction.php
@@ -5,11 +5,14 @@ namespace App\Containers\Storage\Actions;
 use App\Ship\Parents\Actions\Action;
 use Apiato\Core\Foundation\Facades\Apiato;
 use App\Ship\Parents\Requests\Request;
+use Auth;
 
 class DownloadPDFAction extends Action
 {
     public function run(Request $request)
     {
+        $ngo = Apiato::call('NGO@FindNgoByIdTask', [Auth::user()->ngo()->id]);
+        Apiato::call('NGO@CheckHasAccessToNgoTask', [$ngo]);
         $file = Apiato::call('Storage@DownloadPDFTask', [$request]);
         return $file;
     }
--- app/Containers/Storage/Tasks/DownloadPDFTask.php
+++ app/Containers/Storage/Tasks/DownloadPDFTask.php
@@ -4,13 +4,15 @@ namespace App\Containers\Storage\Tasks;
 
 use App\Containers\User\Models\User;
 use App\Ship\Parents\Tasks\Task;
+use Auth;
 use PDF;
 
 class DownloadPDFTask extends Task
 {
     public function run($request)
     {
-        $ngo = User::find(2)->ngo()->first();
+//        $ngo = User::find(2)->ngo()->first();
+        $ngo = Auth::user()->ngo();
         $pdf = PDF::loadView('ngo::timeline-pdf', ['ngo' => $ngo]);
         return $pdf;
     }
